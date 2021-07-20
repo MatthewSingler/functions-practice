73c30818446f7497b8d3931b73b3d3b291e9170b
Matthew Singler <matthew.singler@gmail.com>
2021-07-20 13:13:21 -0500

    Created another function for Best In Show challenge

--- practice.js
+++ practice.js
@@ -8,7 +8,7 @@ const dogAge = calculateAgeInDogYears(45)
 console.log(dogAge)*/
 
 //write a function that will take string as arguement
-const createAFunction = (dogBreed) => {
+/*const createAFunction = (dogBreed) => {
    
     if (dogBreed === "schnauzer") {
        return `My favorite is ${dogBreed}`
@@ -24,4 +24,20 @@ const someOtherValue = createAFunction("meow")
 const thirdDog = createAFunction("Sheepdog")
 console.log(returnValue)
 console.log(someOtherValue)
-console.log(thirdDog)
+console.log(thirdDog)*/
+
+
+const someDog = (dogBreed) => {
+    if (dogBreed === "German Shepherd") {
+        return (`My favorite dog is a ${dogBreed}.`)
+    } else if (dogBreed === "Meow") {
+        return ("I like cats")
+    }
+}
+
+
+someDog("German Shepherd")
+const theOtherAnimal = someDog("")
+/*console.log(theOtherAnimal)*/
+const theCat = someDog("Meow")
+console.log(theCat)
